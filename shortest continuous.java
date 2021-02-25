afb273c71b4fc069310cbcd25826e6abcca75aa4
ayushkal <ayushkal98@gmail.com>
2021-02-25 15:23:15 +0000

    leetcode solution

---
+++ shortest continuous.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int min = 100001, max = -100001;
+        boolean flag = false;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < nums[i - 1])
+                flag = true;
+            if (flag)
+                min = Math.min(min, nums[i]);
+        }
+        flag = false;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] > nums[i + 1])
+                flag = true;
+            if (flag)
+                max = Math.max(max, nums[i]);
+        }
+        int l, r;
+        for (l = 0; l < nums.length; l++) {
+            if (min < nums[l])
+                break;
+        }
+        for (r = nums.length - 1; r >= 0; r--) {
+            if (max > nums[r])
+                break;
+        }
+        
+        if(r - l < 0)
+            return 0;
+        else
+            return r - l +1;
+        
+    }
+}
+
